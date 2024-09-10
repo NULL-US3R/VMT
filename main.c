b9ea3d31f0de11286233370709d27b978a6313b0
NULL-US3R <petrovsergej76279@gmail.com>
2024-09-10 20:23:55 +0300

    Vectors basics

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct vector{
+    int l;
+    float * v;
+};
+
+typedef struct vector * vector;
+
+vector vmt_zero_v(int l){
+    vector v = (vector)malloc(sizeof(struct vector));
+    v->v = (float *)calloc(l, sizeof(float));
+    v->l=l;
+    return v;
+}
+
+vector vmt_rand_v(int l){
+    vector v = (vector)malloc(sizeof(struct vector));
+    v->v = (float *)malloc(l*sizeof(float));
+    v->l=l;
+    for (int i=0; i<l; i++){
+        v->v[i] = ((float)(rand()%1000000))/1000000;
+    }
+    return v;
+}
+
+void vmt_print_v(vector v){
+    for (int l=0; l<v->l; l++){
+        printf("%1.6f ", v->v[l]);
+    }
+    printf("\n");
+}
+
 struct matrix{
     int r, c;
     float * v;
@@ -8,7 +39,7 @@ struct matrix{
 
 typedef struct matrix * matrix;
 
-matrix vmt_zero(int r, int c){
+matrix vmt_zero_m(int r, int c){
     matrix m = (matrix)malloc(sizeof(struct matrix));
     m->v = (float *)calloc(r*c, sizeof(float));
     m->r=r;
@@ -16,7 +47,7 @@ matrix vmt_zero(int r, int c){
     return m;
 }
 
-matrix vmt_rand(int r, int c){
+matrix vmt_rand_m(int r, int c){
     matrix m = (matrix)malloc(sizeof(struct matrix));
     m->v = (float *)malloc(r*c*sizeof(float));
     m->r=r;
@@ -27,7 +58,7 @@ matrix vmt_rand(int r, int c){
     return m;
 }
 
-void vmt_print(matrix m){
+void vmt_print_m(matrix m){
     for (int r=0; r<m->r; r++){
         for (int c=0; c<m->c; c++){
             printf("%1.6f ", m->v[r*m->c+c]);
@@ -36,7 +67,7 @@ void vmt_print(matrix m){
     }
 }
 
-void vmt_write(matrix m, float * a, int s){
+void vmt_write_m(matrix m, float * a, int s){
     for(int i=0; i<s; i++){
         m->v[i]=a[i];
     }
@@ -49,8 +80,7 @@ void vmt_add_mm(matrix a, matrix b, matrix r){
 }
 
 int main(){
-    matrix a = vmt_rand(4, 2);
-
-    vmt_print(a);
+    vector a = vmt_zero_v(4);
+    vmt_print_v(a);
     return 0;
 }
