8ced6b3724f4909e5ec57f26a8ca6fcafeb368d0
NULL-US3R <petrovsergej76279@gmail.com>
2024-09-22 18:43:47 +0300

    ndarray

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct vmt_ndarr{
+    int l;
+    int * d;
+    float * v;
+};
+typedef struct vmt_ndarr * vmt_ndarr;
+
+
+
 int main(){
     return 0;
 }
