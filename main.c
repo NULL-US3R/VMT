32e4583edfab583dff4f2f04ea20e4b76c4d49ed
NULL-US3R <petrovsergej76279@gmail.com>
2024-09-25 12:54:40 +0300

    print fix

--- main.c
+++ main.c
@@ -28,45 +28,67 @@ vmt_ndarr vmt_zero(int l,...){
     return o;
 }
 
-void vmt_printvals(float * v, int * s, int level, int orig){
-    if (level==1){
-        printf("{");
-        for (int i=0; i<s[0]; i++){
-            if (i==0){
-                printf("%1.3f", v[i]);
-            }else{
-                printf(", %1.3f", v[i]);
-            }
-        }
-        printf("}");
-    }else{
-        printf("{");
-        for (int i=0; i<s[0]; i++){
-            if (!(i==0)){
-                printf(",\n");
-                for (int j=0; j<(orig-level+1); j++){
-                    printf(" ");
-                }
-            }
-            vmt_printvals(v+i*s[1], s+1, level-1, orig);
-        }
-        printf("}");
+vmt_ndarr vmt_contig(int l,...){
+    va_list args;
+    vmt_ndarr o = (vmt_ndarr)malloc(sizeof(struct vmt_ndarr));
+    o->dims=l;
+    o->shape = (int *)malloc(l*sizeof(int));
+    va_start(args, l);
+    for (int i=0; i<l; i++){
+        o->shape[i]=va_arg(args, int);
     }
+    va_end(args);
+    int c=1;
+    for (int i=0; i<l; i++){
+        c*=o->shape[i];
+    }
+    o->size=c;
+    o->vals=(float *)malloc(c*sizeof(float));
+    for (int i=0; i<c; i++){
+        o->vals[i] = (float)i;
+    }
+    return o;
 }
 
 void vmt_print(vmt_ndarr a){
-    vmt_printvals(a->vals, a->shape, a->dims, a->dims);
-    printf("\nshape(");
-    for (int i=0; i<a->dims; i++){
-        if (i==0){
-            printf("%i", a->shape[i]);
-        }else{
-            printf(", %i", a->shape[i]);
+    printf("{");
+    for (int i=0; i<a->size; i++){
+        int s=a->size;
+        int count = 0;
+        for (int j=0; j<a->dims-1; j++){
+            s=s/a->shape[j];
+            if (i%(s)==0){
+                count=count+1;
+            }
+        }
+        if (count>0 && !(i==0)){
+            for (int k=0; k<a->dims-count; k++){
+                printf(" ");
+            }
+        }
+        for (int k=0; k<count; k++){
+            printf("{");
+        }
+        printf("%.1f", a->vals[i]);
+        s=a->size;
+        for (int j=0; j<a->dims-1; j++){
+            s=s/a->shape[j];
+            if ((i+1)%(s)==0){
+                printf("}");
+            }
+        }
+        if (!(i==a->size-1)){
+            printf(", ");
+            if ((i+1)%a->shape[a->dims-1]==0){
+                printf("\n");
+            }
         }
     }
-    printf(")\n");
+    printf("}\n");
 }
 
 int main(){
+    vmt_ndarr a = vmt_contig(3, 4, 3, 5);
+    vmt_print(a);
     return 0;
 }
